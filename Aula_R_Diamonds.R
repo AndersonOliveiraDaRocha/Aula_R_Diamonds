7736ef3d0019a6c1e30aafaef5166349c70fdbf6
Anderson Rocha <anderson.rocha@hotmail.com>
2018-11-02 15:13:13 -0300

    Update
    Atualizações no arquivo e inclusão de arquivos de imagem

--- Aula_R_Diamonds.R
+++ Aula_R_Diamonds.R
@@ -1,8 +1,22 @@
 getwd()
+setwd('C:\\Users\\Anderson\\Documents\\UDACITY\\Nanodegree Data Scientist\\Aulas de R\\Diamonds\\Aula_R_Diamonds\\')
 library(ggplot2)
 data(diamonds)
 summary(diamonds)
 ?diamonds
 diamonds$color
-qplot(x=diamonds$price,data=diamonds)
+qplot(x=diamonds$price,data=diamonds, binwidth= 100)+
+  scale_x_continuous(breaks = seq(0,10000,1000),limits = c(0,10000))+
+  facet_wrap(~cut)
+ggsave('Diamonds_Price.png')
 names(diamonds)
+summary(diamonds$price)
+sum(diamonds$price>=15000)
+
+by(diamonds$price,diamonds$cut,summary)
+
+qplot(x=diamonds$price / diamonds$carat,data=diamonds,binwidth=0.01)+
+  scale_x_log10() +
+  facet_wrap(~cut)
+
+diamonds$carat
