58ec682e0abae0346437f1f5f75488d19e9b3858
Anderson Rocha <anderson.rocha@hotmail.com>
2018-11-02 15:45:43 -0300

    Update
    Atualizando com alterações no arquivo sobre o exercício de R

--- Aula_R_Diamonds.R
+++ Aula_R_Diamonds.R
@@ -18,5 +18,21 @@ by(diamonds$price,diamonds$cut,summary)
 qplot(x=diamonds$price / diamonds$carat,data=diamonds,binwidth=0.01)+
   scale_x_log10() +
   facet_wrap(~cut)
+ggsave('Quilate.png')
 
-diamonds$carat
+#Plotando gráfico de CAIXA (BOXPLOT)
+qplot(x = color, y = price, data = diamonds, geom = 'boxplot') +
+  scale_y_continuous(limits = c(0, 15000), breaks = seq(0, 15000, 500))
+
+
+q_max <- max(diamonds$price / diamonds$carat)
+q_min <- min(diamonds$price / diamonds$carat)
+
+qplot(x = color, y = price/carat, data = diamonds, geom = 'boxplot') +
+  scale_y_continuous(limits = c(q_min,q_max), breaks = seq(q_min,q_max, 1500)) +
+  ylab('Quilate')
+
+by(diamonds$price,diamonds$color,summary)
+
+#GRAFICO DE FREQUENCIA
+qplot(x= carat,data= diamonds,geom = 'freqpoly',binwidth=0.01)
